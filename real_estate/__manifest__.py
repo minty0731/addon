10d4b17245046e1a9ecdc1992442af43bdcfcb9f
minty0731 <truongminh3107@gmail.com>
2023-08-18 17:32:32 +0700

    real estate commit

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate Advertisement',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'description': 'Manage Real Estate Advertisements',
+    'depends': ['base'],
+    'data': [],  
+    'demo': [],  
+    'installable': True,
+    'auto_install': False,
+}
---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,24 @@
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sq m)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string="Garden Area (sq m)")
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], string="Garden Orientation")
