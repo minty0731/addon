40e02795a63cb0410d4929aa95350c0a3e6aaf22
minty0731 <truongminh3107@gmail.com>
2023-08-23 17:53:38 +0700

    update23/8

--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import date, timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +8,14 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(string="Available From")
+    date_availability = fields.Date(
+        string="Available From",
+        copy=False,
+        default=(date.today() + timedelta(days=90)).strftime('%Y-%m-%d') 
+    )
     expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)  
     living_area = fields.Integer(string="Living Area (sq m)")
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -22,3 +27,16 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ], string="Garden Orientation")
+    active = fields.Boolean(string='Active', default=True)
+    
+    state = fields.Selection(
+        [('new', 'New'), 
+         ('offer_received', 'Offer Received'), 
+         ('offer_accepted', 'Offer Accepted'), 
+         ('sold', 'Sold'), 
+         ('canceled', 'Canceled')],
+        string='Status',
+        required=True,
+        copy=False,
+        default='new'
+    )
